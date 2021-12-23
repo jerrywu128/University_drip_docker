8d0cb15e2e7bb4ee1bec7c5a39729fe709f5e6e4
jerrywu128 <a0938489128@gmail.com>
2021-12-23 15:08:06 +0800

    drip_platform_server

---
+++ location_list/webapi.py
@@ -0,0 +1,22 @@
+from flask import Flask,Response, render_template, request, jsonify, make_response, send_from_directory, redirect, url_for
+import json
+import pymongo
+
+
+app = Flask(__name__)
+
+
+mongo = pymongo.MongoClient("mongodb://admin:123456@mongo:27017/")
+db = mongo['admin']
+value_col= db['Gps_service']
+
+
+@app.route('/location_list', methods=['GET'])
+@app.route('/', methods=['GET'])
+def data_list():
+    location_value = value_col.find_one({},{ "_id": 0})    
+    return Response(json.dumps(location_value,indent = 4),mimetype='application/json')    
+
+
+if __name__ =='__main__':
+    app.run(host='0.0.0.0',port=5001,debug=True)
